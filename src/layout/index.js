9a146f5defeb91188819ab202c8e007459c0d394
admclamb <adylanmclamb@yahoo.com>
2022-02-03 15:57:51 -0500

    init layout

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 const Layout = ({ weather }) => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/daily" element={<Daily weather={weather} />} />
+        <Route path="/today" element={<Today weather={weather} />} />
+        <Route path="/hourly" element={<Hourly weather={weather} />} />
+        <Route path="/" element={<Home weather={weather} />} />
+        <Route path="/settings" element={<Settings weather={weather} />} />
+      </Routes>
+      <Footer />
     </>
   );
 };
