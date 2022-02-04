e6f271b28ca50916702bedc3b55ca7a51a82b0c5
admclamb <adylanmclamb@yahoo.com>
2022-02-03 21:34:13 -0500

    Update Navbar

---
+++ src/api/getCoordsFromPlace.js
@@ -0,0 +1,15 @@
+export async function getCoordsFromPlace(city) {
+  try {
+    const KEY = process.env.REACT_APP_OPEN_KEY;
+    const limit = 5;
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city},&limit=${limit}&appid=${KEY}`
+    );
+    const responseFromApi = await response.json()[0];
+    console.log(responseFromApi);
+    const { lat, lon } = responseFromApi[0];
+    return { lat, lon };
+  } catch (error) {
+    throw error;
+  }
+}
