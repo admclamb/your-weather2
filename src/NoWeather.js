3b295059965cad5210c85ce6c931a5d14f9bcd23
admclamb <adylanmclamb@yahoo.com>
2022-02-23 22:07:19 -0500

    added inline style to no weather because issue with loading css file

--- src/NoWeather.js
+++ src/NoWeather.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "./footer/Footer";
 import SearchForm from "./header/SearchForm";
-import "./NoWeather.css";
 const NoWeather = ({ setCoords }) => {
   return (
     <>
-      <header className="no-weather-header bg-caution">
+      <header
+        className="no-weather-header bg-caution"
+        style={{ height: "70vh" }}
+      >
         <section className="container">
           <nav className="pt-2 pb-2">
             <a className="navbar-brand">Weatherly</a>
